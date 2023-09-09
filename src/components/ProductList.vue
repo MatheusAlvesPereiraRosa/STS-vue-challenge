9c95d115d27dbf7ced9db0b5a7c863e877da4989
Matheus Alves <matpr.a98@gmail.com>
2023-09-09 10:39:26 -0300

    modificando layout

---
+++ src/components/ProductList.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Lista de Produtos</div>
+</template>
+
+<script setup></script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { computed } from 'vue'
+import store from '../store'
+
+const products = computed(() => store.state.products)
+</script>
+
+<template>
+  <main>
+    <div class="flex my-20 mx-10 justify-center">
+      <input
+        type="text"
+        class="rounded border-2 border-purple-700 w-full"
+        placeholder="Procurar pelos produtos"
+      />
+    </div>
+
+    <div class="mx-10" v-for="product in products" :key="product.id">
+      <h1>{{product.name}}</h1>
+    </div>
+  </main>
+</template>
